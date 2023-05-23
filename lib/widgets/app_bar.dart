bedadc1fb1bf61e5a2de0dd3d94159f0aec65284
Mahdis <mahdis@tooskatech.com>
2023-05-22 20:12:33 -0400

    UI changes for the chat screen

--- lib/widgets/app_bar.dart
+++ lib/widgets/app_bar.dart
@@ -1,30 +1,41 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_svg/flutter_svg.dart';
 
 class CustomAppBar extends StatelessWidget implements PreferredSizeWidget {
   const CustomAppBar({super.key});
 
   @override
-  Size get preferredSize => const Size.fromHeight(kToolbarHeight);
+  Size get preferredSize => const Size.fromHeight(kToolbarHeight + 16.0);
 
   @override
   Widget build(BuildContext context) {
     return AppBar(
       leading: IconButton(
-        icon: const Icon(Icons.menu,
-            color: Color(0xFF2D9CDB)), // Set the color of the burger menu icon
+        icon: SvgPicture.asset(
+          'assets/menu.svg',
+          color: Color.fromARGB(255, 255, 255, 255),
+        ),
         onPressed: () {
           // Open the menu
         },
       ),
-      title: const Text(
-        'identa',
-        style: TextStyle(
-            fontWeight: FontWeight.bold,
-            color: Color(0xFF2D9CDB)), // Set the color of the "identa" text
+      titleSpacing:
+          0.0, // Remove the default spacing between the title and leading icon
+      title: Row(
+        children: [
+          const Text(
+            'identa',
+            style: TextStyle(
+              fontWeight: FontWeight.bold,
+              color: Color.fromARGB(255, 255, 255, 255),
+            ),
+          ),
+        ],
       ),
-      centerTitle: true,
+      centerTitle: false, // Align the title to the left
       elevation: 0.0,
-      backgroundColor: Colors.transparent,
+      backgroundColor: const Color(0xFF2D9CDB),
+      toolbarHeight: kToolbarHeight + 16.0,
     );
   }
 }
--- lib/widgets/chat_bubble.dart
+++ lib/widgets/chat_bubble.dart
@@ -5,7 +5,11 @@ class ChatBubble extends StatelessWidget {
   final String content;
   final bool isMe;
 
-  const ChatBubble({super.key, required this.sender, required this.content, required this.isMe});
+  const ChatBubble(
+      {super.key,
+      required this.sender,
+      required this.content,
+      required this.isMe});
 
   @override
   Widget build(BuildContext context) {
@@ -23,34 +27,33 @@ class ChatBubble extends StatelessWidget {
             ),
           ),
           const SizedBox(height: 4.0),
-          Material(
-            borderRadius: BorderRadius.only(
-              topLeft: isMe ? const Radius.circular(16.0) : Radius.zero,
-              topRight: isMe ? Radius.zero : const Radius.circular(16.0),
-              bottomLeft: const Radius.circular(16.0),
-              bottomRight: const Radius.circular(16.0),
-            ),
-            elevation: 2.0,
-            color: isMe ? const Color(0xFF2D9CDB) : Colors.grey[300],
-            child: Container(
-              decoration: BoxDecoration(
-                borderRadius: BorderRadius.circular(16.0),
-                boxShadow: [
-                  BoxShadow(
-                    color: const Color.fromARGB(255, 46, 44, 44).withOpacity(0.2),
-                    blurRadius: 16.0,
-                    offset: const Offset(0, 8.0),
-                  ),
-                ],
+          Container(
+            decoration: BoxDecoration(
+              borderRadius: BorderRadius.only(
+                topLeft: isMe ? const Radius.circular(16.0) : Radius.zero,
+                topRight: isMe ? Radius.zero : const Radius.circular(16.0),
+                bottomLeft: const Radius.circular(16.0),
+                bottomRight: const Radius.circular(16.0),
               ),
-              child: Padding(
-                padding: const EdgeInsets.symmetric(vertical: 10.0, horizontal: 16.0),
-                child: Text(
-                  content,
-                  style: TextStyle(
-                    fontSize: 16.0,
-                    color: isMe ? Colors.white : Colors.black,
-                  ),
+              boxShadow: [
+                BoxShadow(
+                  color: const Color.fromARGB(0, 4, 4, 4).withOpacity(0.2),
+                  blurRadius: 4.0,
+                  offset: const Offset(0, 4.0),
+                ),
+              ],
+              color: isMe
+                  ? const Color(0xFF2D9CDB)
+                  : const Color.fromARGB(255, 255, 255, 255),
+            ),
+            child: Padding(
+              padding:
+                  const EdgeInsets.symmetric(vertical: 10.0, horizontal: 16.0),
+              child: Text(
+                content,
+                style: TextStyle(
+                  fontSize: 16.0,
+                  color: isMe ? Colors.white : Colors.black,
                 ),
               ),
             ),
