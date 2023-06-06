51db3223707153bb272e17335e718c543abff74e
arash <arash@tooskatech.com>
2023-06-05 21:11:46 -0400

    remove unused imports

--- lib/widgets/note_content.dart
+++ lib/widgets/note_content.dart
@@ -1,8 +1,5 @@
-import 'dart:convert';
-
 import 'package:flutter/material.dart';
 import 'package:intl/intl.dart';
-import 'package:shared_preferences/shared_preferences.dart';
 import '../services/apis/api.dart';
 import 'note_model.dart';
 
