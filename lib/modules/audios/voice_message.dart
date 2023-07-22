90e7150492798ef45834fc751f16507761494f0b
mersad karimi <mersad@tooskatech.com>
2023-07-22 20:28:39 +0330

    create ShowCustomDialog file and refactor note , VoiceMessage

--- lib/modules/audios/voice_message.dart
+++ lib/modules/audios/voice_message.dart
@@ -1,15 +1,16 @@
 import 'package:flutter/material.dart';
 import 'package:identa/core/extensions/context_extension.dart';
+import 'package:identa/core/models/model_core/note_model.dart';
 import 'package:identa/core/repositories/note_provider.dart';
 import 'package:identa/modules/audios/audioPlayer/audio_player_card.dart';
 import 'package:identa/modules/audios/audioPlayer/audio_player_logic.dart';
 import 'package:identa/widgets/dismissible_background.dart';
+import 'package:identa/widgets/show_custom_dialog.dart';
 import 'package:provider/provider.dart';
 
 class VoiceMessage extends StatefulWidget {
-  const VoiceMessage({
-    super.key,
-  });
+  const VoiceMessage({super.key, this.note});
+  final NoteModel? note;
   @override
   State<VoiceMessage> createState() => _VoiceMessageState();
 }
@@ -17,53 +18,83 @@ class VoiceMessage extends StatefulWidget {
 class _VoiceMessageState extends State<VoiceMessage> {
   @override
   Widget build(BuildContext context) {
-    final audioRecordsProvider = Provider.of<NoteProvider>(context);
-    final updatedAudioRecords = audioRecordsProvider.updatedAudioRecords;
-    return ListView.builder(
-      shrinkWrap: true,
-      physics: const NeverScrollableScrollPhysics(),
-      itemCount: updatedAudioRecords.length,
-      itemBuilder: (context, index) {
-        final audioRecord = updatedAudioRecords[index];
-        return Padding(
-          padding: const EdgeInsets.only(top: 8.0),
-          child: Dismissible(
-            key: Key(audioRecord.toString()),
-            background: const DismissibleBackground(),
-            onDismissed: (_) {
-              //context.read<MyAudioRecordsLogicInterface>().delete(audioRecord);
-              context.notify = 'Audio record dismissed';
-            },
-            child: Provider<AudioPlayerLogicInterface>(
-              create: (_) => AudioPlayerLogic(
-                audioPath: audioRecord.audioPath,
-              ),
-              dispose: (_, logic) => logic.onDispose(),
-              child: Row(
-                children: [
-                  AudioPlayerCard(
-                    audioRecord,
-                    key: Key(audioRecord.audioPath),
-                  ),
-                  IconButton(
-                    onPressed: () {
-                      // context
-                      //     .read<MyAudioRecordsLogicInterface>()
-                      //     .delete(audioRecord);
-                      context.notify = 'Audio record dismissed';
-                    },
-                    icon: const Icon(
-                      Icons.delete,
-                      color: Color.fromARGB(255, 250, 121, 112),
-                      size: 22,
+    final noteProvider = Provider.of<NoteProvider>(context);
+    final updatedAudioRecords = noteProvider.updatedAudioRecords;
+    return SafeArea(
+      child: ListView.builder(
+        shrinkWrap: true,
+        physics: const NeverScrollableScrollPhysics(),
+        itemCount: updatedAudioRecords.length,
+        itemBuilder: (context, index) {
+          final audioRecord = updatedAudioRecords[index];
+          return Padding(
+            padding: const EdgeInsets.all(10),
+            child: Dismissible(
+              key: Key(audioRecord.toString()),
+              background: const DismissibleBackground(),
+              onDismissed: (_) {
+                String fileId = Uri.parse(audioRecord.audioPath)
+                    .pathSegments
+                    .last
+                    .replaceAll('.m4a', '');
+                print("pathId $fileId");
+                Provider.of<NoteProvider>(context, listen: false)
+                    .deleteNoteAudio(widget.note!, fileId);
+                context.notify = 'Audio record dismissed';
+              },
+              confirmDismiss: (_) async {
+                return await ShowCustomDialog.show(
+                  context,
+                  'Delete Voice',
+                  'Are you sure you want to delete this voice?',
+                );
+              },
+              child: Provider<AudioPlayerLogicInterface>(
+                create: (_) => AudioPlayerLogic(
+                  audioPath: audioRecord.audioPath,
+                ),
+                dispose: (_, logic) => logic.onDispose(),
+                child: Row(
+                  children: [
+                    AudioPlayerCard(
+                      audioRecord,
+                      key: Key(audioRecord.audioPath),
                     ),
-                  ),
-                ],
+                    IconButton(
+                      padding: const EdgeInsets.only(left: 15),
+                      onPressed: () async {
+                        String fileId = Uri.parse(audioRecord.audioPath)
+                            .pathSegments
+                            .last
+                            .replaceAll('.m4a', '');
+
+                        bool? shouldDelete = await ShowCustomDialog.show(
+                          context,
+                          'Delete Audio',
+                          'Are you sure you want to delete this audio recording?',
+                        );
+
+                        if (shouldDelete == true) {
+                          Provider.of<NoteProvider>(context, listen: false)
+                              .deleteNoteAudio(widget.note!, fileId);
+                          ScaffoldMessenger.of(context).showSnackBar(
+                            SnackBar(content: Text('Audio record deleted')),
+                          );
+                        }
+                      },
+                      icon: const Icon(
+                        Icons.delete,
+                        color: Color.fromARGB(255, 250, 121, 112),
+                        size: 22,
+                      ),
+                    ),
+                  ],
+                ),
               ),
             ),
-          ),
-        );
-      },
+          );
+        },
+      ),
     );
   }
 }
